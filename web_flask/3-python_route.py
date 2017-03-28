e961e4c9f92d388f50859ef88d9c13675cabd50a
Jimmy Tran <jimmy.tran@holbertonschool.com>
2017-03-28 22:12:37 +0000

    Pep8, 2 routes

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -1,4 +1,27 @@
 #!/usr/bin/python3
-REPLACE
-    return (0)
-print(REPLACE)
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello_HBNB():
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb')
+def HBNB():
+    return "HBNB"
+
+
+@app.route('/c/<text>')
+def C(text):
+    return "c %s" % text
+
+
+@app.route('/python/')
+@app.route('/python/<text>')
+def python(text="is cool"):
+    return "python %s" % text
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
