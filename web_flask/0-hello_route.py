3b6bddb72152b59aa72fa987c5705f76181136b0
Jimmy Tran <jimmy.tran@holbertonschool.com>
2017-03-28 21:32:10 +0000

    Pep8, works now

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,4 +1,11 @@
 #!/usr/bin/python3
-REPLACE
-    return (0)
-print(REPLACE)
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello_HBNB():
+    return "Hello HBNB!"
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
