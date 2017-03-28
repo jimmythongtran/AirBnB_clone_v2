a7af6951206b32a0f0357748e3591e6ebdb65881
Jimmy Tran <jimmy.tran@holbertonschool.com>
2017-03-28 21:48:56 +0000

    Pep8, works

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -1,4 +1,21 @@
 #!/usr/bin/python3
-REPLACE
-    return (0)
-print(REPLACE)
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello_HBNB():
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb')
+def HBNB():
+    return "HBNB"
+
+
+@app.route('/c/<text>')
+def C(text):
+    return "c %s" % text
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
